46ddc55a1ec9397c0a05ab1400fef074a1699add
dlamego_mc4 <dlamego.mc4@digio.com.br>
2023-02-12 16:31:41 -0300

    upload do projeto

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { Players } from '@screens/Players';
+import { StatusBar } from 'react-native';
+import theme from './src/theme';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      {fontsLoaded ?
+        <Players /> : <Loading />
+      }
+
+    </ThemeProvider>
+  );
+}
+
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,23 @@
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
+import { Container } from "./styles";
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+      <Highlight
+        title="Nome da turma"
+        subtitle="Adicione a galera e separe os times"
+      />
+
+      <Input
+        placeholder="Nome da pessoa"
+        autoCorrect={false}
+      />
+      <ButtonIcon icon={"add"} />
+    </Container>
+  )
+}
