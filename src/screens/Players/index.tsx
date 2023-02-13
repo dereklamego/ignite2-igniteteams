3c48235637be1253591432c529775bb6c4538798
dlamego_mc4 <dlamego.mc4@digio.com.br>
2023-02-13 09:25:28 -0300

    feature: header list created

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -2,9 +2,16 @@ import { ButtonIcon } from "@components/ButtonIcon";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
-import { Container } from "./styles";
+import { Filter } from "@components/Filter";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -13,11 +20,35 @@ export function Players() {
         subtitle="Adicione a galera e separe os times"
       />
 
-      <Input
-        placeholder="Nome da pessoa"
-        autoCorrect={false}
-      />
-      <ButtonIcon icon={"add"} />
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+        <ButtonIcon icon={"add"} />
+      </Form>
+
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B", "Time C", "Time D"]}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+
+        />
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
+
+
     </Container>
   )
 }
